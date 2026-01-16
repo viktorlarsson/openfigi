58e474e68df02f4250281307b3177430daa3b7ba
viktorlarsson <viktorsarstrom@gmail.com>
2026-01-16 21:29:52 +0100

    fix: Swap to bun test

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'bun:test'
+import { describe, expect, it } from 'vitest'
 import { exponentialBackoff, parseRateLimitHeaders, sleep } from '../src/utils/helpers'
 
 describe('Utils', () => {
